4e9e077cc0bd4bbfaa3947566a25785010a5a919
budtkin-ship-it <budtkin@gmail.com>
2026-06-17 15:32:10 +0300

    Improve test names

--- tests/first-own-test.spec.ts
+++ tests/first-own-test.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('open Playwright website and check title', async ({ page }) => {
+test('Playwright homepage should have correct title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('open example.com and check heading', async ({ page }) => {
+test('Example.com page should have correct heading', async ({ page }) => {
   await page.goto('https://example.com/');
 
   await expect(page.getByRole('heading', { name: 'Example Domain' })).toBeVisible();
