5cd7f04a2376783bd29d8e8b6fcea19e3b63e361
budtkin-ship-it <budtkin@gmail.com>
2026-06-18 12:38:10 +0300

    Replace training specs with SauceDemo login test

--- tests/playwright-homepage.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Playwright homepage should have correct title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  await expect(page).toHaveTitle(/Playwright/);
-});
